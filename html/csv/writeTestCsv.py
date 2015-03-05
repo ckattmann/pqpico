41a8371ea7888e370bd1729afd3291772fc06202
kipfer <christoph.kattmann@googlemail.com>
2015-03-05 21:11:32 +0100

    Deleted unnecessary files

--- html/csv/writeTestCsv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def writecsv():
-    with open('data.csv','wb') as csvfile:
-        datawriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        datawriter.writerow(['spam'] * 5)
-
--- html/jsontest/jsonwriter.py
+++
@@ -1,5 +0,0 @@
-import json
-
-data = [1,2,3,4,5,6,7]
-
-print('getjsondata({'+str(json.dumps(data))+'})')
